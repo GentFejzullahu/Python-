03d9e37366264b70bdbfa121f4f6c0fe42e165a2
GentFejzullahu <gentfejzullahu576@gmail.com>
2025-10-23 16:16:46 +0200

    Add files via upload

---
+++ Module 1/index.py
@@ -0,0 +1,28 @@
+#print('Hello')
+
+# temp=98.6
+# name='Melika'
+#
+# print(name)
+#
+# print(tybe(name))
+# print(tybe(temp))
+#
+# print('The best student in class is',name)
+#
+# x=5
+# y=2
+# result=x+y
+# print(result)
+
+shopping_list=['appell','orange','Gent']
+
+print(shopping_list[2])
+
+shopping_list.append('milk')\
+
+shopping_list.insert( 2,'joe')
+
+shopping_list.remove('appell')
+shopping_list.pop(1)
+print(shopping_list)
