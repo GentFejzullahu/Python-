deb3febce5a91a4fcbd6665bf671b5e34328ffaa
GentFejzullahu <gentfejzullahu576@gmail.com>
2025-10-28 16:03:37 +0100

    Add files via upload

---
+++ Module 2/tuples.py
@@ -0,0 +1,38 @@
+# Tuples
+# words=("spam","white","black")
+#
+# print(words(2))
+#
+# person=("Alice",30,"strujagji")
+#
+# name,age,profession=person
+#
+# print(name," is ","profession is",profession,"and she is",age,"years old")
+
+# dictionaries
+
+# contact_info={
+#     "Alice":"045-758-924",
+#     "Bob Builder":"044-123-522"
+#
+# }
+#
+# Alice_phone=contact_info['Alice']
+# print(Alice_phone)
+#
+# print(contact_info)
+#
+# # del contact_info("Bob Builder")
+# # print(contact_info)
+#
+# keys=contact_info.keys()
+# print(keys)
+#
+# values=contact_info.values()
+# print(values)
+#
+# items=contact_info.items()
+# print()
+
+
+
