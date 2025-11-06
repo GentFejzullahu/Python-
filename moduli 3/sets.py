3ff78d200e68bd781cc97710ebb3ecd1fb4eb484
GentFejzullahu <gentfejzullahu576@gmail.com>
2025-11-06 15:56:24 +0100

    commit

---
+++ moduli 3/sets.py
@@ -0,0 +1,90 @@
+# my_set = {1, 2, 3}
+# my_set = set()
+# my_set = set([4, 5, 6])
+#
+# print(my_set)
+from pydantic.v1.utils import unique_list
+
+#
+# set1 = {1, 2, 3}
+# set2 = {3, 4, 5}
+#
+# union_result_method = set1.union(set2)
+# print("Union: ", union_result_method)
+#
+# union = set1 | set2
+# print("Union method: ", union)
+#
+# intersection = set1 & set2
+# print("intersection method: ", intersection)
+#
+# difference = set1 - set2
+# print("difference method: ", difference)
+#
+# symmetrical_difference = set1 ^ set2
+# print("symmetrical_difference method: ", symmetrical_difference)
+
+#Set methods
+
+# my_set = {1, 2, 3}
+#
+# my_set.add(4)
+#
+# my_set.remove(3)
+#
+# my_set.discard(3)
+#
+# my_set_clear()
+#
+# print (my_set)
+# set_length =len(my_set)
+# print("length on set:", set_length)
+
+#Using sets - removing duplicates
+
+# my_list = [1, 2, 2, 9, 9, 6, 7]
+#
+# unique_set = set(my_list)
+#
+# unique_list = list(unique_set)
+#
+# print(unique_list)
+
+#in and not in
+#
+# loyalty_members = {"Ensar","Lisi","Arti"}
+# costomer  = "Lisi"
+#
+# is_member = costomer in loyalty_members
+#
+# print(is_member)
+
+# age = 19
+#
+# if age >= 19:
+#     print("you can vote")
+# else:
+#     print("you cannot vote yet")
+
+# temp = 29
+#
+# if temp > 30:
+#     print("its a very VERY HOT DAY")
+# elif 20 <= temp <= 30:
+#     print("nice")
+# else:
+#     print("its a good day")
+
+# student_gpa = 4.5
+# student_score = 75
+# if student_gpa >= 3.5:
+#      print(f"Students with GPA {student_gpa} and test score of {student_score} Mirë je so keq")
+# elif student_score > 65:
+#     print(f"Students with GPA {student_gpa} and test score of {student_score} Brave ke arrit në jet")
+# else:
+#      print(f"Students with GPA {student_gpa} and test score of {student_score} Sje kerkah me jet")
+#
+# else:
+# print(f"Students with GPA {student_gpa} and test score of {student_score} mey be eligible for a patial scholarship")
+
+
