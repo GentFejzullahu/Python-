d59380140b775bcf339eea2770bf1930388c9b7c
GentFejzullahu <gentfejzullahu576@gmail.com>
2025-11-11 16:04:14 +0100

    commit1.1

---
+++ Moduli 4/loops.py
@@ -0,0 +1,60 @@
+# names = ["Alice","Bobi","Builderi"]
+#
+# for  name in names:
+#     print(name)
+#
+#
+# senrence = "hello, world"
+#
+# for character in sentence:
+#     if character.isalpha():
+#         print (character)
+
+#     numbers = [12, 45, 11, 20, 1, 2]
+#
+#     maximum = numbers [0]
+#
+#     for num in numbers:
+#         if num > maximum:
+#             maximum = num
+# print("The maximum number value in the list is:", maximum)
+
+# count = 1
+#
+# while count <= 5:
+#     print("Interation: ", count)
+#     count += 1
+
+# numbers= [1, 2, 3, 4, 5]
+# target = 4
+#
+# for number in numbers:
+#     print(number)
+#     if number == target:
+#         print("target found")
+#         break
+
+# scores = [23, 44, 55, 76]
+# total = 0
+# count = 0
+#
+# for score in scores:
+#     if score <50:
+#         continue
+#     total += score
+#     count += 1
+#
+# average = total / count if count > 0 else 0
+#
+# print("Average score above 50", average)
+
+# while True:
+#
+#     user_input = input("Enter a positive number: ")
+#     if user_input.isnumeric():
+#         number = int(user_input)
+#         if number > 0:
+#             break
+#     print("Invalid input. try again: ")
+# print("you entered a valid positive number")
+#
